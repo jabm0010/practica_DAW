2e6c7bc26279860ae2fcb24af0d80f3b308f4a38
jabm0010 <jabm0010@red.ujaen.es>
2018-04-01 12:50:58 +0200

    VideojuegosDAOJDBC completado

--- src/main/java/com/daw/vj/dao/VideojuegosDAOJDBC.java
+++ src/main/java/com/daw/vj/dao/VideojuegosDAOJDBC.java
@@ -7,6 +7,7 @@ package com.daw.vj.dao;
 
 import com.daw.vj.model.Videojuego;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,6 +26,9 @@ import javax.sql.DataSource;
  */
 public class VideojuegosDAOJDBC implements VideojuegoDAO {
 
+    private static final String SQL_BUSCAID = "SELECT * FROM Videojuegos where videojuegoID=?";
+    private static final String SQL_BUSCANOMBRE = "SELECT * FROM Videojuegos WHERE nombreVideojuego LIKE ?";
+    private static final String SQL_BUSCATODOS = "SELECT * FROM Videojuegos";
     private DataSource ds;
 
     public VideojuegosDAOJDBC() {
@@ -39,7 +43,7 @@ public class VideojuegosDAOJDBC implements VideojuegoDAO {
 
     }
 
-    private static Videojuego mensajeMapper(ResultSet rs) throws SQLException {
+    private static Videojuego videojuegoMapper(ResultSet rs) throws SQLException {
         Videojuego v;
         v = new Videojuego(rs.getInt("videojuegoID"),
                 rs.getString("nombreVideojuego"),
@@ -52,18 +56,20 @@ public class VideojuegosDAOJDBC implements VideojuegoDAO {
 
     @Override
     public List<Videojuego> buscaNombre(String nombre) {
-        String SQL_BUSCATODOS = "Select * from Videojuegos";
         List<Videojuego> videojuegos = new ArrayList<Videojuego>();
-        try (
-                Connection conn = ds.getConnection(); //Obtenemos conexión del pool de conexiones
-                Statement st = conn.createStatement();
-                ResultSet rs = st.executeQuery(SQL_BUSCATODOS);) {
-            while (rs.next()) {
-                videojuegos.add(mensajeMapper(rs));
+        Videojuego v = null;
+        try (Connection conn = ds.getConnection();
+                PreparedStatement stmn = conn.prepareStatement(SQL_BUSCANOMBRE)) {
+            stmn.setString(1, "%" + nombre + "%");
+            try (ResultSet rs = stmn.executeQuery()) {
+                while (rs.next()) {
+                    videojuegos.add(videojuegoMapper(rs));
+                }
             }
         } catch (SQLException ex) {
-            Logger.getLogger(VideojuegosDAOJDBC.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(VideojuegosDAOJDBC.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
         }
+
         return videojuegos;
 
     }
@@ -80,19 +86,31 @@ public class VideojuegosDAOJDBC implements VideojuegoDAO {
 
     @Override
     public Videojuego buscaId(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Videojuego v = null;
+        try (Connection conn = ds.getConnection();
+                PreparedStatement stmn = conn.prepareStatement(SQL_BUSCAID)) {
+            stmn.setInt(1, id);
+            try (ResultSet rs = stmn.executeQuery()) {
+                rs.next();
+                v = videojuegoMapper(rs);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VideojuegosDAOJDBC.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        }
+        return v;
+
     }
 
     @Override
     public List<Videojuego> buscaTodos() {
-        String SQL_BUSCATODOS = "Select * from Videojuegos";
+
         List<Videojuego> videojuegos = new ArrayList<Videojuego>();
         try (
                 Connection conn = ds.getConnection(); //Obtenemos conexión del pool de conexiones
                 Statement st = conn.createStatement();
                 ResultSet rs = st.executeQuery(SQL_BUSCATODOS);) {
             while (rs.next()) {
-                videojuegos.add(mensajeMapper(rs));
+                videojuegos.add(videojuegoMapper(rs));
             }
         } catch (SQLException ex) {
             Logger.getLogger(VideojuegosDAOJDBC.class.getName()).log(Level.SEVERE, null, ex);
